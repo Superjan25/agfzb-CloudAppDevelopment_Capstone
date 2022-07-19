c98aca6e643ea647891bd3a9d3e1405df18be8e1
Superjan25 <jan.scherer1@gmail.com>
2022-07-19 23:35:51 +0000

    models created

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,27 +1,37 @@
 from django.db import models
 from django.utils.timezone import now
 
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=100, default='Make')
+    description = models.CharField(max_length=500)
 
-# Create your models here.
+    def __str__(self):
+        return "Name: " + self.name
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    MINIVAN = 'Minivan'
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (MINIVAN, 'Minivan')
+    ]
 
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=100, default='Model')
+    dealerId = models.IntegerField(default=1)
+    type = models.CharField(choices=CAR_TYPES, max_length=10, default=SEDAN)
+    year = models.DateField()
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
-
+    def __str__(self):
+        return "Name: " + self.name + ", " + \
+            "Dealer Id " + str(self.dealerId) + ", " + \
+            "Type: " + self.type + ", " + \
+            "Year" + str(self.year)
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
