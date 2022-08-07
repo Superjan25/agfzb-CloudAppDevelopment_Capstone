f47c7d67996d9f7d3ca8f811a63bf1ff8d667716
Superjan25 <jan.scherer1@gmail.com>
2022-08-07 16:13:07 +0000

    addreview page

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -40,7 +40,7 @@ def login_request(request):
         if user is not None:
             # If user is valid, call login method to login current user
             login(request, user)
-            return redirect('djangoapp:index')
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
         else:
             # If not, return to login page again
             return render(request, 'djangoapp/user_login.html', context)
@@ -54,7 +54,7 @@ def logout_request(request):
     # Logout user in the request
     logout(request)
     # Redirect user back to course list view
-    return redirect('djangoapp:index')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 # Create a `registration_request` view to handle sign up request
 def registration_request(request):
@@ -115,7 +115,13 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 def add_review(request, dealer_id):
-    if request.method == "POST":
+    context = dict()
+    if request.method == "GET":
+        url = "https://65987e9d.eu-gb.apigw.appdomain.cloud/api/dealership"
+        dealership = get_dealer_by_id_from_cf(url, dealer_id)
+        context["dealer"] = dealership
+        return render(request, 'djangoapp/add_review.html', context)
+    elif request.method == "POST":
         if request.user.is_authenticated:
             url = "https://65987e9d.eu-gb.apigw.appdomain.cloud/api/review"
             review = dict()
